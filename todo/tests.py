124dbf9be044bd103a13a3b18deddda7df4e371f
1F10240134 <s1f102401348@iniad.org>
2025-07-03 11:39:58 +0900

    警告の修正

--- todo/tests.py
+++ todo/tests.py
@@ -5,10 +5,14 @@ from todo.models import Task
 
 # Create your tests here.
 class SampleTestCase(TestCase):
+
+
     def test_sample(self):
         self.assertEqual(1 + 2, 3)
     
 class TaskModelTestCase(TestCase):
+
+
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59,59))
         task = Task(title='task1', due_at=due)
@@ -49,10 +53,12 @@ class TaskModelTestCase(TestCase):
             current3 = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
             task = Task(title='task1', due_at=due)
             task.save()
-
+            
             self.assertFalse(task.is_overdue(current3))
 
 class TodoViewTestCase(TestCase):
+
+
     def test_index_get(self):
         client = Client()
         response = client.get('/')
