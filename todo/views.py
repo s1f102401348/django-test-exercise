3b2f725ac0b5be5ea369542cb12979500b19e32d
1F10240134 <s1f102401348@iniad.org>
2025-07-11 10:02:35 +0900

    views.pyの文字抜けを修正

--- todo/views.py
+++ todo/views.py
@@ -48,7 +48,7 @@ def updete(request, task_id):
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-    if request.method == 'POST'
+    if request.method == 'POST':
     task.title = request.POST['title']
     task.due_at = make_aware(parse_datetime(request.POST['due_at']))
     task.save()
